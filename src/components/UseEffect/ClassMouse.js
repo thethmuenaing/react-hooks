b3f465f60e3027a61a148137d4f9e392b0584f19
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-08-15 15:06:12 +0630

    adding the clean up function to ClassMouse.js and HookMouse.js and adding the code of MouseContainer.js

--- src/components/UseEffect/ClassMouse.js
+++ src/components/UseEffect/ClassMouse.js
@@ -18,6 +18,10 @@ class ClassMouse extends Component {
 		window.addEventListener("mousemove", this.logMousePosition);
 	}
 
+	componentWillUnmount() {
+		window.removeEventListener("mousemove", this.logMousePosition);
+	}
+
 	render() {
 		return (
 			<div>
