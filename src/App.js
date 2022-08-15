e4e06718558a01fc2d000de01044e797728b7393
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-08-15 14:33:27 +0630

    adding the  some function of UseEffect code to HookMouse.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
-import ClassMouse from "./components/UseEffect/ClassMouse";
+import HookMouse from "./components/UseEffect/HookMouse";
 function App() {
 	return (
 		<div className="App">
-			<ClassMouse />
+			<HookMouse />
 		</div>
 	);
 }
