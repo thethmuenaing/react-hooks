c3da158adb73c0d7b416681cd6ab40e382edd9e2
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-08-16 22:23:24 +0630

    adding the code of UseReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import ComponentA from "./components/Context/ComponentA";
+import CounterOne from "./components/UseReducer/CounterOne";
 
 function App() {
 	return (
 		<div className="App">
-			<ComponentA />
+			<CounterOne />
 		</div>
 	);
 }
