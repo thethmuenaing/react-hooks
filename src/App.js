94f21bbe54f0392b2aa58bcbdbc41c0988294f17
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-08-16 09:01:51 +0630

    adding the code of DataFetching.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
-import IntervalClassCounter from "./components/UseEffect/IntervalClassCounter";
+import DataFetching from "./components/UseEffect/DataFetching";
 function App() {
 	return (
 		<div className="App">
-			<IntervalClassCounter />
+			<DataFetching />
 		</div>
 	);
 }
