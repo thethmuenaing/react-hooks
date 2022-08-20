9c40342777acc32084b1e73b891989b70ecc4721
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-08-20 18:14:57 +0630

    using the UseRef hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import ClassTimer from "./components/UseRef/ClassTimer";
+import HookTimer from "./components/UseRef/HookTimer";
 
 function App() {
 	return (
 		<div className="App">
 			<ClassTimer />
+			<HookTimer />
 		</div>
 	);
 }
